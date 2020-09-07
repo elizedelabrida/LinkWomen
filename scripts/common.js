f6e58f71c33969f674f39759d494f8b52e945303
Bruna Gadelha <bruna_gadelha@live.com>
2020-09-07 15:32:49 -0300

    Ajustes gerais

--- scripts/common.js
+++ scripts/common.js
@@ -1,23 +1,31 @@
 // Definine carousel skip time
-$('#carousel-events').carousel({
-    interval: 2000
-})
+$("#carousel-events").carousel({
+  interval: 2000,
+});
 
 // Define carousel cards quantity
-$('.carousel .carousel-item').each(function () {
-    let next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    if ($(window).innerWidth() >= 768) {
-        for (let i = 0; i < 2; i++) {
-            next = next.next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
+$(".carousel .carousel-item").each(function () {
+  let next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
+  if ($(window).innerWidth() >= 768) {
+    for (let i = 0; i < 2; i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(":first");
+      }
 
-            next.children(':first-child').clone().appendTo($(this));
-        }
+      next.children(":first-child").clone().appendTo($(this));
     }
+  }
 });
+
+function handleLogout() {
+  localStorage.removeItem("linkwomen:userId");
+  localStorage.removeItem("linkwomen:token");
+  localStorage.removeItem("linkwomen:discudiscussionIdssionId");
+
+  location.href="login.html"; 
+}
